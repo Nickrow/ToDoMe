7f92145b8b42827cb7c3884e73c766bf268fc24f
Nickrow <nikolay1905@gmail.com>
2025-11-25 17:53:33 +0300

    Initial Commit

---
+++ ToDoMe/ToDoMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoMeApp.swift
+//  ToDoMe
+//
+//  Created by Николай Воробьев on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
